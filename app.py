76987d313e905d9f70a857219942f45fcc4ae140
KenEkanem <kennedyekanem@gmail.com>
2024-09-04 21:57:58 +0100

    update app.py with checkindate time

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 import requests
+from datetime import datetime
 
 app = Flask(__name__)
 
 # Your Google Apps Script URL
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx8o0uVWqb809GKNUv7n1i5ESGLR3F8U_vTwLH7rSYlqPs_T0jK6Kj1o-ZXF6iPlEbqig/exec"
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwFYRcSvnTV4wBpc5-n4UqJ_OHPUa13zonEM0sCBS7ybEOnAve7sIAMGz8pvQ-705ZpUw/exec"
+
+# Set the check-in start date
+CHECKIN_START_DATE = datetime(2024, 9, 26, 9, 0)
 
 @app.route('/checkin-by-barcode', methods=['GET', 'POST'])
 def checkin_by_barcode():
@@ -16,6 +20,11 @@ def checkin_by_barcode():
         # Ensures both barcode and email are provided
         if not unique_id or not email:
             return "unique_id and email are required", 400
+
+        # Check the current date
+        current_date = datetime.now()
+        if current_date < CHECKIN_START_DATE:
+            return render_template('date_checkin.html'), 200
 
         # Sends request to the Apps Script to search by barcode and email
         try:
@@ -42,11 +51,15 @@ def checkin_by_barcode():
     # If not a GET request, return the form
     return render_template('email_checkin.html')
 
-
 @app.route('/checkin-by-email', methods=['GET', 'POST'])
 def checkin_by_email():
     if request.method == 'POST':
         email = request.form.get('email')
+
+        # Check the current date
+        current_date = datetime.now()
+        if current_date < CHECKIN_START_DATE:
+            return render_template('date_checkin.html'), 200
 
         # Sends request to the Apps Script to search by email
         try:
